cec6298a49ae4cedb8c8ad5de7f54915027d045e
Chris-Denmark <cdenmark97@gmail.com>
2021-01-14 10:53:38 -0600

    added event listener in Nutshell.js to get the login/registration pages to disappear after logging in

--- src/scripts/Nutshell.js
+++ src/scripts/Nutshell.js
@@ -1,3 +1,11 @@
 export const Nutshell = () => {
     // Render all your UI components here
 }
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".auth")
+
+eventHub.addEventListener("userAuthenticated", clickEvent => {
+    contentTarget.innerHTML = Nutshell()
+})
+
